e834bff383444bc155746eead9cf295f49f45820
Francisco <francisco.raverta@gmail.com>
2025-05-27 16:28:58 -0300

    Change library dependencies for fork in lrse github.

--- SuperBuild/cmake/External-RenderDEM.cmake
+++ SuperBuild/cmake/External-RenderDEM.cmake
@@ -8,7 +8,7 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/lrse/SRenderDEM
+  GIT_REPOSITORY    https://github.com/FranciscoRaverta/SRenderDEM
   GIT_TAG           main
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
